063dff67d34dfea54226ba5bbf60e75330ad6982
Brian Lawrence <brianlawrence2@gmail.com>
2014-05-05 23:24:19 -0500

    Moved keys

--- marvel.go
+++ marvel.go
@@ -14,8 +14,6 @@ const (
   marvelAPIURL = "http://gateway.marvel.com/v1/public/"
   userAgent    = "go-marvel" + version
   version      = "0.1.0"
-  publicKey    = "061a906efd7ee7415f72f239b61a7157"
-  privateKey   = "a5c7f840d6059d9584189d6370d8a480e60731b0"
 )
 
 func Hash(pubKey string, privKey string, ts string) string {
@@ -29,7 +27,7 @@ func Url(pubKey string, privKey string, ts string, endpoint string) string {
   return marvelAPIURL + endpoint + "?ts=" + ts + "&apikey=" + pubKey + "&hash=" + Hash(pubKey, privKey, ts)
 }
 
-func GetCharacters(endpoint string) (*http.Response, error) {
+func GetCharacters(publicKey, privateKey, endpoint string) (*http.Response, error) {
   t    := time.Now().Format("20060102150405")
   //hash := Hash(publicKey, privateKey, t)
 
